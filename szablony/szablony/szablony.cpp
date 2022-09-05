2c8889dfce1e93a5a37e14ded543f5968748d3a7
Nbartek <64834216+Nbartek@users.noreply.github.com>
2022-09-05 12:25:28 +0200

    xn
    fjyjflkyhlh

--- szablony/szablony/szablony.cpp
+++ szablony/szablony/szablony.cpp
@@ -1,6 +1,6 @@
 // szablony.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <string>
 #include <iostream>
 
 template <typename T>
@@ -12,9 +12,20 @@ template <typename T>
 T obwPr(T pBok1, T pbok2) {
     return (2*pbok2 + 2 * pBok1);
 }
+template <typename J>
+J obwKol(J promien) {
+    return(2 * promien * 3.14);
+}
+template<typename J>
+J polKol(J promien) {
+    return(3.14 * (promien*promien));
+}
 int main()
 {
     float pbok1 = 1.23, pbok2 = 3.2;
     std::cout << obwPr<int>(pbok1,pbok2) << std::endl;
+    std::cout << polePr<float>(pbok1, pbok2) << std::endl;
+    std::cout << obwKol( pbok2) << std::endl;
+    std::cout << polKol(pbok1) << std::endl;
 }
 
