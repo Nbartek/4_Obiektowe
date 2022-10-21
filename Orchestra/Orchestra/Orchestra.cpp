fe6a99267bf0a954e2a4a33cf333beef644041b5
Nbartek <64834216+Nbartek@users.noreply.github.com>
2022-10-21 09:39:49 +0200

    dgvsd
    dsfvs

--- Orchestra/Orchestra/Orchestra.cpp
+++ Orchestra/Orchestra/Orchestra.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -27,6 +29,36 @@ public:
         cout << "Trombone play ..." << endl;
     }
 };
+class Orchestra : public Instrument {
+public:
+    Orchestra(){}
+    Orchestra(string name) {
+        setName(name);
+    }
+    ~Orchestra() {
+        for (size_t i = 0; i < instruments.size(); i++)
+        {
+            delete instruments[i];
+            
+        }
+        instruments.clear();
+    }
+    void play() {
+        for(Instrument* var : instruments)
+        {
+            var->play();
+        }
+    }
+    void setName(string name) {
+        this->name = name;
+    }
+    void addInstrument(Instrument* instrument) {
+        instruments.push_back(instrument);
+    }
+private:
+    vector<Instrument*> instruments;
+    string name;
+};
 
 
 int main()
@@ -36,11 +68,11 @@ int main()
     Instrument* flute = new Flute;
     Instrument* trombone = new Trombone;
 
-    Instrument* orchestra[] = { guitar, flute, trombone };
-
-    for (Instrument* instrument : orchestra)
-    {
-        instrument->play();
-    }
-
+    
+    Orchestra orchestra;
+    orchestra.setName("gerdzio");
+    orchestra.addInstrument(guitar);
+    orchestra.addInstrument(flute);
+    orchestra.addInstrument(trombone);
+    orchestra.play();
 }
