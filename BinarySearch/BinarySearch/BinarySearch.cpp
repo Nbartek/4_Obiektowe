ff1dbb070e8c164a21fb50de32a4a74ae380dfe7
Nbartek <64834216+Nbartek@users.noreply.github.com>
2022-11-08 13:24:15 +0100

    estesd
    sfds

--- BinarySearch/BinarySearch/BinarySearch.cpp
+++ BinarySearch/BinarySearch/BinarySearch.cpp
@@ -30,6 +30,38 @@ public:
 		}
 		return -1;
 	}
+	void fillNumbers() {
+		for (int i = 0; i < length; i++) {
+			array.push_back(rand() / 100);
+		}
+	}
+	void bubbleSort() {
+		int i, j;
+		float temp;
+		for ( i = 0; i < length; i++)
+		{
+			for (j = i + 1; j < length; j++) {
+				if (array[j] < array[i]) {
+					temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+				}
+			}
+		}
+	}
+	void merge(vector<int> tablica, int poczatek, int srodek, int koniec) {
+		int indexTab1 = srodek - poczatek + 1;
+		int indexTab2 = koniec - srodek;
+
+		
+	}
+	void mergeSort(vector<int> tablica, int początek, int koniec) {
+		if (początek > koniec) return;
+		int srodek = (początek + koniec) / 2;
+		mergeSort(tablica, początek, srodek);
+		mergeSort(tablica, srodek + 1, koniec);
+		merge(tablica, początek, srodek, koniec);
+	}
 	void display() {
 		if (find_Number() == -1) {
 		cout << "Nie znaleznion liczby" << endl;
@@ -38,23 +70,24 @@ public:
 			cout << " Znaleziono na miejscu: " << find_Number() << endl;
 		}
 	}
+	
 };
-int find_Number(vector<int> vec,int length,int searchItem) {
-	int start = 0;
-	int end = length - 1;
-	while (start<=end)
-	{
-		int middle = (start + end) / 2;
-		if (vec[middle] == searchItem) return middle;
-		else {
-			if (vec[middle] > searchItem) { end = middle - 1; }
-			else{
-				start = middle + 1;
-			}
-		}
-	}
-	return -1;
-}
+//int find_Number(vector<int> vec,int length,int searchItem) {
+//	int start = 0;
+//	int end = length - 1;
+//	while (start<=end)
+//	{
+//		int middle = (start + end) / 2;
+//		if (vec[middle] == searchItem) return middle;
+//		else {
+//			if (vec[middle] > searchItem) { end = middle - 1; }
+//			else{
+//				start = middle + 1;
+//			}
+//		}
+//	}
+//	return -1;
+//}
 int main()
 {
 	vector<int> a = { 11,23,35,44,51,59,62,71,88,91 };
