7c858e1564ebc60689b4dbf5edd51c25a9ca987e
Nbartek <64834216+Nbartek@users.noreply.github.com>
2022-11-28 12:29:50 +0100

    3w53
    3wr

--- QuickSort/QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort/QuickSort.cpp
@@ -2,6 +2,7 @@
 //
 #include <iostream>
 #include <time.h>
+#include <windows.h>
 
 void quickSort(int* tablica, int lewo, int prawo) {
     int pivot = tablica[(lewo + prawo) / 2];
@@ -22,26 +23,69 @@ void quickSort(int* tablica, int lewo, int prawo) {
     if(j>lewo) quickSort(tablica, lewo, j);
     if(i<prawo)quickSort(tablica, i, prawo);
 }
+void sortowanieBabelkowe(int t[], int n) {
+    int i, j;
+    int temp;
+    for (i = 0; i < n; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (t[j] < t[i]) {
+                temp = t[i];
+                t[i] = t[j];
+                t[j] = temp;
+            }
+        }
+    }
+}
 int main()
 {
-    int ile = 20;
+    clock_t start, stop;
+    double czas;
+    int ile = 1000000;
     int* tablica;
-    tablica = new int[20];
+    int* tablica2;
+    tablica = new int[ile];
+    tablica2 = new int[ile];
     srand(time(NULL));
     for (size_t i = 0; i < ile; i++)
     {
         tablica[i] = rand() % 100 + 1;
+        tablica2[i] = rand() % 100 + 1;
+
     }
-    for (size_t i = 0; i < ile; i++)
-    {
-        std::cout << " " << tablica[i] << " ";
-    }
-    std::cout<<"--------------"<<std::endl;
+    //for (size_t i = 0; i < ile; i++)
+    //{
+    //    std::cout << " " << tablica[i] << " ";
+    //}
+    //std::cout<<"--------------"<<std::endl;
+    std::cout << "Sortowanie szybkie" << std::endl;
+    start = clock();
     quickSort(tablica, 0, ile-1);
-    for (size_t i = 0; i < ile; i++)
-    {
-        std::cout<<" "<<tablica[i]<<" ";
-    }
+    stop = clock();
+    czas = (double)(stop - start) / CLOCKS_PER_SEC;
+    std::cout << czas << std:: endl;
+    //for (size_t i = 0; i < ile; i++)
+    //{
+    //    std::cout<<" "<<tablica[i]<<" ";
+
+    //}
+    std::cout << "Tablica bombelkowa" << std::endl;
+    //for (size_t i = 0; i < ile; i++)
+    //{
+    //    std::cout << " " << tablica2[i] << " ";
+    //}
+    //std::cout << "--------------" << std::endl;
+    start = clock();
+    sortowanieBabelkowe(tablica, ile);
+    stop = clock();
+    czas = (double)(stop - start) / CLOCKS_PER_SEC;
+    std::cout << czas << std::endl;
+    //for (size_t i = 0; i < ile; i++)
+    //{
+    //    std::cout << " " << tablica2[i] << " ";
+
+    //}
 }
 
 
